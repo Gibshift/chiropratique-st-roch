aaacb022a3950e6f74434eb2462f70907fc0f180
Gibshift <alexandre.theoret@hotmail.com>
2026-06-19 21:27:05 -0400

    Rename categories admin labels

--- src/collections/Categories.ts
+++ src/collections/Categories.ts
@@ -4,21 +4,31 @@ import { anyone } from '../access/anyone'
 import { authenticated } from '../access/authenticated'
 import { slugField } from 'payload'
 
-export const Categories: CollectionConfig = {
+export const Categories: CollectionConfig<'categories'> = {
   slug: 'categories',
+
+  labels: {
+    singular: 'Catégorie',
+    plural: 'Catégories',
+  },
+
   access: {
     create: authenticated,
     delete: authenticated,
     read: anyone,
     update: authenticated,
   },
+
   admin: {
+    group: 'Blogue',
     useAsTitle: 'title',
   },
+
   fields: [
     {
       name: 'title',
       type: 'text',
+      label: 'Titre',
       required: true,
     },
     slugField({
