cda2f73020ff91d5ebb98819f46158b10cb178eb
Gibshift <alexandre.theoret@hotmail.com>
2026-06-22 10:35:37 -0400

    Update sitemap configuration for clinic site

--- next-sitemap.config.cjs
+++ next-sitemap.config.cjs
@@ -1,18 +1,32 @@
-const SITE_URL =
+const rawSiteUrl =
   process.env.NEXT_PUBLIC_SERVER_URL ||
   process.env.VERCEL_PROJECT_PRODUCTION_URL ||
-  'https://example.com'
+  'https://www.chiropratiquestroch.com'
+
+const SITE_URL = rawSiteUrl.startsWith('http') ? rawSiteUrl : `https://${rawSiteUrl}`
 
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: SITE_URL,
   generateRobotsTxt: true,
-  exclude: ['/posts-sitemap.xml', '/pages-sitemap.xml', '/*', '/posts/*'],
+
+  exclude: [
+    '/admin/*',
+    '/api/*',
+    '/next/*',
+    '/posts',
+    '/posts/*',
+    '/search',
+    '/pages-sitemap.xml',
+    '/posts-sitemap.xml',
+  ],
+
   robotsTxtOptions: {
     policies: [
       {
         userAgent: '*',
-        disallow: '/admin/*',
+        allow: '/',
+        disallow: ['/admin/*', '/api/*', '/next/*'],
       },
     ],
     additionalSitemaps: [`${SITE_URL}/pages-sitemap.xml`, `${SITE_URL}/posts-sitemap.xml`],
